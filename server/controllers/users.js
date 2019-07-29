8e83c0ae5c5d8f77fda8ba72838a2b940824b625
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-07-29 13:43:42 +0800

    updated node-3, added token

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,28 +1,32 @@
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const secret = require('../../secret.js');
 function create(req, res) {
    const db = req.app.get('db');
-   const {email, password} = req.body;
- 
-db.users
-  .insert(
-    {
-      email,
-      password,
-      user_profiles: [
+   const { email, password} = req.body;
+
+  argon2
+    .hash(password)
+    .then(hash => {
+      return db.users.insert(
         {
-          userId: undefined,
-          about: null,
-          thumbnail: null,
+          email,
+          password: hash,
         },
-      ],
-    },
-    {
-      deepInsert: true,
-    }
-  )
-  .then(user => res.status(201).json(user))
-  .catch(err => {
-    console.error(err);
-  });
+        {
+          fields: ['id', 'email'],
+        }
+      );
+    })
+    .then(user => {
+      const token = jwt.sign({ userId: user.id }, secret); // adding token generation
+      res.status(201).json({ ...user, token });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    }); 
+
 }
 
 function list(req, res) {
@@ -62,10 +66,70 @@ function getProfile (req, res) {
 
 }
 
+function getData (req, res) {
+  if (!req.headers.authorization){
+	return res.status(401).end();
+  }
+
+  try {
+   const token = req.headers.authorization.split(' ')[1];
+   jwt.verify(token, secret);
+   res.status(200).json({ data: 'here is the protected data' });
+  } catch (err) {
+   console.error(err);
+   res.status(401).end(); 
+  }
+}
+
+function login (req, res){
+  const db = req.app.get('db');
+  const { email, password } = req.body;
+
+  db.users
+    .findOne(
+      {
+        email,
+      },
+      {
+        fields: ['id', 'email', 'password'],
+      }
+    )
+    .then(user => {
+      if (!user) {
+        throw new Error('Invalid email');
+      }
+
+      // Here is where we check the hashed password from the database
+      // with the password that was submitted by the user.
+      return argon2.verify(user.password, password).then(valid => {
+        if (!valid) {
+          throw new Error('Incorrect password');
+        }
+
+        const token = jwt.sign({ userId: user.id }, secret);
+        delete user.password; // remove password hash from returned user object
+        res.status(200).json({ ...user, token });
+      });
+    })
+    .catch(err => {
+      if (
+        ['Invalid email', 'Incorrect password'].includes(err.message)
+      ) {
+        res.status(400).json({ error: err.message });
+      } else {
+        console.error(err);
+        res.status(500).end();
+      }
+    });
+
+}
+
 module.exports = {
  create,
  list,
  getById,
  getProfile,
+ getData,
+ login
 };
 
